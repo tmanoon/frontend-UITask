64e0dd8403f45016ad0b87d7f2d77d55fbe10c69
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-16 12:25:26 +0300

    Add basic routing and cmps

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import { useEffect, useState } from 'react'
+import { AppHeader } from './cmps/AppHeader'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
 
 function App() {
 
   return (
-    <>
-      <h1>Starter</h1>
-    </>
+    <Provider store={store}>
+      <Router>
+        <AppHeader />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
-}
 
+}
 export default App
