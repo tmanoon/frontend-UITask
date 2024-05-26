9eb16c14c0786ff3e52f5fed3786015c3821af98
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-26 13:41:25 +0300

    Add class in button to match styling of button

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -11,7 +11,7 @@ export function AppHeader() {
                     <NavLink to='/about'>About</NavLink>
                 </div>
             </div>
-            <button className="contact-btn">Contact us</button>
+            <button className="contact-btn primary-light">Contact us</button>
         </section>
     )
 }
