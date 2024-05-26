f906c55b3686375cf2e41bd6763eda32da15f726
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-26 15:07:52 +0300

    Add shapes and styling properties to match design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,31 @@ export function Home() {
                         <path fillRule="evenodd" clipRule="evenodd" d="M100 156H96V160H100V156ZM89 156H93V160H89V156ZM89 164H93V168H89V164ZM93 172H89V176H93V172ZM89 180H93V184H89V180ZM93 188H89V192H93V188ZM89 196H93V200H89V196ZM86 156H82V160H86V156ZM86 164H82V168H86V164ZM82 172H86V176H82V172ZM86 180H82V184H86V180ZM82 188H86V192H82V188ZM86 196H82V200H86V196ZM100 164H96V168H100V164ZM79 156H75V160H79V156ZM79 164H75V168H79V164ZM75 172H79V176H75V172ZM79 180H75V184H79V180ZM75 188H79V192H75V188ZM79 196H75V200H79V196ZM100 172H96V176H100V172ZM72 156H68V160H72V156ZM72 164H68V168H72V164ZM68 172H72V176H68V172ZM72 180H68V184H72V180ZM68 188H72V192H68V188ZM72 196H68V200H72V196ZM100 180H96V184H100V180ZM65 156H61V160H65V156ZM65 164H61V168H65V164ZM61 172H65V176H61V172ZM65 180H61V184H65V180ZM61 188H65V192H61V188ZM65 196H61V200H65V196ZM100 188H96V192H100V188ZM58 156H54V160H58V156ZM58 164H54V168H58V164ZM54 172H58V176H54V172ZM58 180H54V184H58V180ZM54 188H58V192H54V188ZM58 196H54V200H58V196ZM100 196H96V200H100V196ZM51 156H47V160H51V156ZM51 164H47V168H51V164ZM47 172H51V176H47V172ZM51 180H47V184H51V180ZM47 188H51V192H47V188ZM51 196H47V200H51V196Z" fill="#F67E7E" />
                     </svg>
                 </div>
+                    
                 <div className="text grid">
+                    <svg className="top-shape" width="50" height="4" viewBox="0 0 50 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="4" fill="#79C8C7" />
+                    </svg>
                     <h1>Find the best <span className="talent-word">talent</span></h1>
-                    <p>Finding the right people and building high
+                    <p>
+
+                        Finding the right people and building high
                         performing teams can be hard. Most companies
                         aren’t tapping into the abundance of global talent.
                     </p>
+                </div>
+                <div className="bottom-shape">
+                    <svg width="358" height="100" viewBox="0 0 358 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g id="Group 7">
+                            <g id="Group 4">
+                                <circle id="Oval" cx="100" cy="100" r="100" fill="#2C6269" />
+                                <path id="Combined Shape" fill-rule="evenodd" clip-rule="evenodd" d="M258 200C313.228 200 358 155.228 358 100C358 44.7715 313.228 0 258 0C202.772 0 158 44.7715 158 100C158 155.228 202.772 200 258 200ZM258 133C276.225 133 291 118.225 291 100C291 81.7746 276.225 67 258 67C239.775 67 225 81.7746 225 100C225 118.225 239.775 133 258 133Z" fill="#012F34" />
+                                <rect id="Rectangle" x="358" y="43" width="43" height="43" transform="rotate(180 358 43)" fill="#79C8C7" />
+                                <path id="Combined Shape Copy" fill-rule="evenodd" clip-rule="evenodd" d="M233.513 200C288.742 200 333.513 155.228 333.513 100C333.513 44.7715 288.742 0 233.513 0C178.285 0 133.513 44.7715 133.513 100C133.513 155.228 178.285 200 233.513 200ZM233.513 133C251.739 133 266.513 118.225 266.513 100C266.513 81.7746 251.739 67 233.513 67C215.288 67 200.513 81.7746 200.513 100C200.513 118.225 215.288 133 233.513 133Z" fill="#F67E7E" />
+                            </g>
+                            <path id="Combined Shape_2" fill-rule="evenodd" clip-rule="evenodd" d="M100 0H0V4H100V0ZM100 8H0V12H100V8ZM0 16H100V20H0V16ZM100 24H0V28H100V24ZM0 32H100V36H0V32ZM100 40H0V44H100V40Z" fill="#79C8C7" />
+                        </g>
+                    </svg>
                 </div>
             </article>
         </section>
