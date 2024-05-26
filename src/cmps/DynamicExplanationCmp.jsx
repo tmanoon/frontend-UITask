b4b32906092d5c93c0e68da95d87ad4853c034fd
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-26 16:30:20 +0300

    Add dynamic explanation cmp

--- src/cmps/DynamicExplanationCmp.jsx
+++ src/cmps/DynamicExplanationCmp.jsx
@@ -1,9 +1,11 @@
 export function DynamicExplanationCmp({ shape, header, text }) {
     return (
-        <div className="explanation-container">
-            <h2> {header} </h2>
+        <div className="explanation-container flex align-center">
             {shape}
-            <p> {text} </p>
+            <div className="text flex column">
+                <h2> {header} </h2>
+                <p> {text} </p>
+            </div>
         </div>
     )
 }
