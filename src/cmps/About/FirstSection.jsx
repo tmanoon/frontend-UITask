6b3a5fe8e2b05cfeaf324572af7159f47861c368
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-28 17:58:23 +0300

    Add first section cmp of about page

---
+++ src/cmps/About/FirstSection.jsx
@@ -0,0 +1,13 @@
+export function FirstSection() {
+    return (
+        <article className="first-section grid">
+            <h1>About</h1>
+            <div className="shape-and-txt">
+                <svg width="50" height="4" viewBox="0 0 50 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="50" height="4" fill="#F67E7E" />
+                </svg>
+                <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring those teams to you.</p>
+            </div>
+        </article>
+    )
+}
