35f2e7f17dc3c3311250cd017d682f0dcdaecc64
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-29 19:25:37 +0300

    Add second section cmp to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import { FirstSection } from "../cmps/About/FirstSection";
+import { SecondSection } from "../cmps/About/SecondSection";
 
 export function About() {
     return (
         <section className="about">
             <FirstSection />
+            <SecondSection />
         </section> 
     )
 }
