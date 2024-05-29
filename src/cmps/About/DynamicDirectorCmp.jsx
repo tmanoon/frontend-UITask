c0c0add7ac62c1a7673e4ca64693fed394e777dc
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-29 19:23:58 +0300

    Remove txt from props since it is the same text for each director

--- src/cmps/About/DynamicDirectorCmp.jsx
+++ src/cmps/About/DynamicDirectorCmp.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react"
 
-export function DynamicDirectorCmp( {picDetails, fullname, txt}) {
-
+export function DynamicDirectorCmp( {picDetails, fullname}) {
     const [isDirectorClicked, setIsDirectorClicked] = useState( {fullname: '', isClicked: false})
     return (
         <div className="director">
             <img src={`https://xsgames.co/randomusers/assets/avatars/${picDetails.gender}/${picDetails.num}.jpg`} title={`${fullname} picture`} alt={`${fullname} picture`} />
             <h3>{fullname}</h3>
-            <p>"{txt}"</p>
-            <button className="open-close-btn"></button>
+            <p>"Empowered teams create truly amazing products. Set the north star and let them follow it."</p>
+            <button className="open-close-btn" id={fullname}>+</button>
         </div>
     )
 }
