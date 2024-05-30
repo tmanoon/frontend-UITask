5814d751411b1723b1e17476518d46556af21f12
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-30 15:43:12 +0300

    Add fourth section to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,13 @@
 import { FirstSection } from "../cmps/About/FirstSection";
 import { SecondSection } from "../cmps/About/SecondSection";
+import { FourthSection } from "../cmps/FourthSection";
 
 export function About() {
     return (
         <section className="about">
             <FirstSection />
             <SecondSection />
+            <FourthSection />
         </section> 
     )
 }
