095ba6f2f13386d86d414938e81949b23d4d0063
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-05-30 14:37:38 +0300

    Add styling properties via classes

--- src/cmps/About/DynamicDirectorCmp.jsx
+++ src/cmps/About/DynamicDirectorCmp.jsx
@@ -3,11 +3,11 @@ import { useState } from "react"
 export function DynamicDirectorCmp( {picDetails, fullname}) {
     const [isDirectorClicked, setIsDirectorClicked] = useState( {fullname: '', isClicked: false})
     return (
-        <div className="director">
+        <div className="director flex column align-center">
             <img src={`https://xsgames.co/randomusers/assets/avatars/${picDetails.gender}/${picDetails.num}.jpg`} title={`${fullname} picture`} alt={`${fullname} picture`} />
             <h3>{fullname}</h3>
             <p>"Empowered teams create truly amazing products. Set the north star and let them follow it."</p>
-            <button className="open-close-btn" id={fullname}>+</button>
+            <span className="open-close-btn flex center" id={fullname}>+</span>
         </div>
     )
 }
