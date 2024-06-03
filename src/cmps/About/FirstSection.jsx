5bd062821bae7f12f0fe9b9f599b61b04cf1e549
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-06-03 18:06:26 +0300

    Add container for text for styling purposes

--- src/cmps/About/FirstSection.jsx
+++ src/cmps/About/FirstSection.jsx
@@ -1,12 +1,14 @@
 export function FirstSection() {
     return (
-        <article className="first-section grid">
-            <h1>About</h1>
-            <div className="shape-and-txt flex column space-between">
-                <svg width="50" height="4" viewBox="0 0 50 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="50" height="4" fill="#F67E7E" />
-                </svg>
-                <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring those teams to you.</p>
+        <article className="first-section">
+            <div className="text grid">
+                <h1>About</h1>
+                <div className="shape-and-txt flex column space-between">
+                    <svg width="50" height="4" viewBox="0 0 50 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="50" height="4" fill="#F67E7E" />
+                    </svg>
+                    <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring those teams to you.</p>
+                </div>
             </div>
             <svg className="right-shape" width="100" height="200" viewBox="0 0 100 200" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="100" y="100" width="100" height="100" transform="rotate(180 100 100)" fill="#2C6269" />
